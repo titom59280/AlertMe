aced19c0d33a74d7af5b0e366775acee647b80ff
Thomas Graveleine <tgraveleine@gmail.com>
2015-01-16 22:23:09 +0100

    work on pop up message for new user

--- app/src/main/java/com/alertme/projet/alertme/Home.java
+++ app/src/main/java/com/alertme/projet/alertme/Home.java
@@ -1,6 +1,8 @@
 package com.alertme.projet.alertme;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,10 +11,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class Home extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,7 @@ public class Home extends Activity {
         final Button connexion = (Button) findViewById(R.id.btnValider);
         final EditText loginText = (EditText) findViewById(R.id.username);
         final EditText mdpText = (EditText) findViewById(R.id.mdp);
+        final TextView newUser = (TextView) findViewById(R.id.new_user);
 
         connexion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +31,14 @@ public class Home extends Activity {
                 String log = loginText.getText().toString();
                 String pass = mdpText.getText().toString();
                 checkLogin(log,pass);
+            }
+        });
+
+        newUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newUserFragment = new CreateNewUserFragment();
+                newUserFragment.show(getFragmentManager(),"create");
             }
         });
     }
