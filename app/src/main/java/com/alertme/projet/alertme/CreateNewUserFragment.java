5133a013bfc63f35f65a7505f834d19060508972
Thomas Graveleine <tgraveleine@gmail.com>
2015-01-18 14:20:38 +0100

    work on settings

--- app/src/main/java/com/alertme/projet/alertme/CreateNewUserFragment.java
+++ app/src/main/java/com/alertme/projet/alertme/CreateNewUserFragment.java
@@ -6,25 +6,29 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.LayoutInflater;
+import android.widget.EditText;
 
 /**
  * Created by Thomas on 16/01/2015.
  */
 public class CreateNewUserFragment extends DialogFragment {
+    private EditText newUser;
+    private EditText newMdp;
     @Override
     public Dialog onCreateDialog(Bundle saveInstanceState){
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         builder.setView(inflater.inflate(R.layout.new_user,null));
-        builder.setPositiveButton(R.string.create, new DialogInterface.OnClickListener() {
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
-                Intent intent = new Intent(getActivity(), SettingWelcomePage.class);
-                startActivity(intent);
+                    //nothing
+
             }
         })
-                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener(){
-                    public void onClick(DialogInterface dialog, int id){
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
                         //nothing
                     }
                 });
